2bda88685292778bb5d2f8a53df9d79147c40e73
Blake Griffith <blake.a.griffith@gmail.com>
2015-02-10 13:21:10 -0800

    Tests for bug 4753

--- numpy/core/tests/test_multiarray.py
+++ numpy/core/tests/test_multiarray.py
@@ -2276,15 +2276,15 @@ class TestBinop(object):
                     return "ufunc"
                 else:
                     inputs = list(inputs)
-                    inputs[i] = np.asarray(self)
+                    if i < len(inputs):
+                        inputs[i] = np.asarray(self)
                     func = getattr(ufunc, method)
+                    if ('out' in kw) and (kw['out'] is not None):
+                        kw['out'] = np.asarray(kw['out'])
                     r = func(*inputs, **kw)
-                    if 'out' in kw:
-                        return r
-                    else:
-                        x = SomeClass2(r.shape, dtype=r.dtype)
-                        x[...] = r
-                        return x
+                    x = SomeClass2(r.shape, dtype=r.dtype)
+                    x[...] = r
+                    return x
 
         arr = np.array([0])
         obj = SomeClass()
@@ -2327,6 +2327,23 @@ class TestBinop(object):
         kw = np.add(a, [1], signature='ii->i')
         assert_('sig' not in kw and 'signature' in kw)
         assert_equal(kw['signature'], 'ii->i')
+
+    def test_out_override(self):
+        # regression test for github bug 4753
+        class OutClass(ndarray):
+            def __numpy_ufunc__(self, ufunc, method, i, inputs, **kw):
+                if 'out' in kw:
+                    tmp_kw = kw.copy()
+                    tmp_kw.pop('out')
+                    func = getattr(ufunc, method)
+                    kw['out'][...] = func(*inputs, **tmp_kw)
+
+        A = np.array([0]).view(OutClass)
+        B = np.array([5])
+        C = np.array([6])
+        np.multiply(C, B, out=A)
+        assert_equal(A[0], 30)
+        assert_(isinstance(A, OutClass))
 
 
 class TestCAPI(TestCase):
